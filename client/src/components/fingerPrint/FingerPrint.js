b3c59d30a4503dfd570078e54eb2611c2a266638
Liora Yacob <liorayacob10@gmail.com>
2021-05-23 15:56:18 +0300

    refactor: Bio Pass

--- client/src/components/fingerPrint/FingerPrint.js
+++ client/src/components/fingerPrint/FingerPrint.js
@@ -34,68 +34,80 @@ export const FingerPrint = ({ user }) => {
   return (
     <Content>
       <div className={styles.fatherDiv}>
-        {user.Biopass_Taken === false ?
-          <div>
-            <Row gutter={[24, 16]}>
-              <Col span={12}>
-                <Title level={3}> הרכשת אצבע</Title>
-                <br />
+        <div>
+          <Row gutter={[24, 16]}>
+            <Col span={12}>
+              <Title level={3}> הרכשת אצבע</Title>
+              <br />
+              {user.Biopass_Taken === false ?
                 <p>
                   להשלמת התהליך יש לגשת לדגום טביעת אצבע בב"מ.
-            <br />
-            חשוב להשלים זאת עד ה <strong>31.03.2021</strong>
+                  <br />
+                  חשוב להשלים זאת עד ה <strong>31.03.2021</strong>
                 </p>
-                <br />
-              </Col>
-              <Col span={12} />
-            </Row>
-            <br />
+                :
+                <p>
+                  <strong>יש ברשותנו את טביעת האצבע שלך</strong>
+                  <br />
+                </p>
+              }
+              <br />
+            </Col>
+            <Col span={12} />
+          </Row>
+          <br />
       לנוחותך, להלן רשימת המקומות ושעות הפעילות בהם ניתן לדגום אצבע:
       <Row gutter={[24, 16]} className={styles.tailRow}>
-              <Col span={6} className={styles.tile} style={{ textAlign: "center" }}>
-                <Title level={5}>מועד אחרון להשלמה</Title>
+            <Col span={6} className={styles.tile} style={{ textAlign: "center" }}>
+              <Title level={5}>מועד אחרון להשלמה</Title>
+
+              {user.Biopass_Taken === false ?
                 <Title level={2} className={styles.mark}>
                   31.03.2021
-          </Title>
-                <img src={process.env.PUBLIC_URL + "/img/FP.png"} />
-              </Col>
-              <Col span={6} className={styles.tile}>
-                <OperationTime title={<Title level={5}>ב"מ ירקון</Title>} />
-                <OperationTime
-                  title="שעות פעילות:"
-                  icon="/img/Time.png"
-                  firstLine="א-ה 12:35 - 08:00"
-                  secondLine="יום ו 15:00 - 16:00"
-                />
-                <br />
-                <OperationTime
-                  title="טלפון:"
-                  icon="/img/Call.png"
-                  firstLine="03-5289745"
-                  secondLine="054-5864771"
-                />
-              </Col>
-              <Col span={6} className={styles.tile}>
-                <OperationTime title={<Title level={5}>ב"מ מפקדה</Title>} />
-                <OperationTime
-                  title="שעות פעילות:"
-                  icon="/img/Time.png"
-                  firstLine="א-ה 12:35 - 08:00"
-                  secondLine="יום ו 15:00 - 16:00"
-                />
-                <br />
-                <OperationTime
-                  title="טלפון:"
-                  icon="/img/Call.png"
-                  firstLine="03-5289745"
-                  secondLine="054-5864771"
-                />
-              </Col>
-            </Row>
-          </div>
+                </Title>
+                :
+                <Title level={2} className={styles.mark}>
+                  בוצע!
+                </Title>
+              }
+              <img src={process.env.PUBLIC_URL + "/img/FP.png"} style={{ display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            </Col>
+            <Col span={6} className={styles.tile}>
+              <OperationTime title={<Title level={5}>ב"מ ירקון</Title>} />
+              <OperationTime
+                title="שעות פעילות:"
+                icon="/img/Time.png"
+                firstLine="א-ה 12:35 - 08:00"
+                secondLine="יום ו 15:00 - 16:00"
+              />
+              <br />
+              <OperationTime
+                title="טלפון:"
+                icon="/img/Call.png"
+                firstLine="03-5289745"
+                secondLine="054-5864771"
+              />
+            </Col>
+            <Col span={6} className={styles.tile}>
+              <OperationTime title={<Title level={5}>ב"מ מפקדה</Title>} />
+              <OperationTime
+                title="שעות פעילות:"
+                icon="/img/Time.png"
+                firstLine="א-ה 12:35 - 08:00"
+                secondLine="יום ו 15:00 - 16:00"
+              />
+              <br />
+              <OperationTime
+                title="טלפון:"
+                icon="/img/Call.png"
+                firstLine="03-5289745"
+                secondLine="054-5864771"
+              />
+            </Col>
+          </Row>
+        </div>
 
-          : <div><h1>יש טביעת אצבע</h1></div> //TODO
-        }
+
         <br />
         <Row justify="end" align="bottom" className={styles.tailRow}>
           <div>
